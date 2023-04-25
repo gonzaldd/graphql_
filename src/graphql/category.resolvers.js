3f23f1c26fdb62520e08def972b7d1abc3fe9c66
Gonzalo <gonzalodedomingo@gmail.com>
2023-04-25 16:43:11 -0300

    Add auth in createCategory

--- src/graphql/category.resolvers.js
+++ src/graphql/category.resolvers.js
@@ -1,9 +1,19 @@
+const boom = require('@hapi/boom');
+
 const CategoryService = require('../services/category.service');
 const categoryServiceInstance = new CategoryService()
 
 const categories = () => categoryServiceInstance.find({})
 
-const createCategory = (_, {name, image}) => categoryServiceInstance.create({name, image})
+const createCategory = async(_, {name, image}, context ) => {
+    const { user } = await context.authenticate('jwt', { session: false });
+
+    if(!user){
+        throw boom.unauthorized()
+    }
+    
+    return await categoryServiceInstance.create({name, image})
+}
 
 
 module.exports = {
